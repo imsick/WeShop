e6c40867f2ce9ec4b8d988339d6526c11e958fea
llx <244332658@qq.com>
2018-07-20 14:39:41 +0800

    充值部分修改

--- src/main/java/com/beng/webbe/controller/AccountController.java
+++ src/main/java/com/beng/webbe/controller/AccountController.java
@@ -21,8 +21,11 @@ import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sun.rmi.runtime.Log;
 
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -51,20 +54,44 @@ public class AccountController {
     ) {
 
         AlipayClient alipayClient = new
-                DefaultAlipayClient("https://openapi.alipaydev.com/gateway.do", "2016091900545639", "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC9eF/0HNakuQBshAv9re/YEYlSei6zzDQ3BAQxbrtuIUW04fqrLF/y+krALR0FLTmpL9WdFoCuSkxAyZj/2qFFzK2mhaOohM8B0dsJXae8MQHSOQAC4TlyubMBJ8w+oDxOye7Zrd8T4IuMqBUrtkLLSjyfrqUGvhEUkfNw9Cs0NrcTsVZZY+hV8nOdisf/zb7fsC7b4f8HtiX2UxuqWK8W86OTmiFKZa9Lj7oien4IT9UcNKX6W/vGT6yKtAtdYLgcSDBFWsq68Wsi/eVaS2jeqh9SiUXIUFY2kEjDjPjg+wMVtL/JJnVh+csgTHiSdRrtE8rr7Iu5JEAB2qhhJxFBAgMBAAECggEBAIOKHbvBhbcohUXRSu3cowXz7s1m7COBYsLLNAoJoXBtAxL4TGooTZjH8buH1nzwj/aIshn0NHRMOrJEA8elRo/J8MkKQLvPMFRIGHRhJAGAWI5Vak+Vg6fFiqQDuEXGQvhEM2HG13faTafiTYhiae7u5DXMLuAGdp88ULGbop8FJxz/XVS4KFRsifqMML4W5oLMmL6MpRladwZd/WlxazB2d9BE3EWjx9hzhrg3TI4f/q0RkkTXH4/7+4GT+Z7WrwK+FPfK3yJ13vpphbkGQrielnu+Y23Pd50ZRU1VKGZeJ+QLkxebNsq9MG9HhqzqzhSEK0CzwW7CRoyVDGIJnZECgYEA6pxkHcy1HXNypdYYwhdIYuuGGGwjNP7J/H6NCkeIcAiSuyKEjCiyv7Z/39k2npZwY+g9QMgpoWn4v+k/9j8Igh6M0pR1RukrxV9K1EjP2ErNDMos5PC+8ufK2I2M8zW+f+tNh9gKnUtYPLtwoXPFFbwdu2JUfUuAt6pUjbMlKz0CgYEAzr5wvTdcHdJaF8AKPC4OIV3ivCck9nYxCKezO00LpbZXuVaS90tYk8SNl7nm4R5/DNfCCY4UsBRsmbiZIFD5yEFSjqXaRgnilQYmtoMqiIsrPDB7N1WS6+vnMH47ACbnVkVLlJTCZE0M+IBfXwaCAEzOKKv4yU3J8WUgFyPh7lUCgYBYUzJ56ZSSFuGlv3mosJ1G6IaecS+8BuRCU96DbTTXgNLE8xTVs4jAFlQEEUG36Mk9Q7Szy+eoZj4I+iZjbY2ldxzrNosEfEpHTz3bt+HP+zFBTGgkdOJejpdWecwD0Er827hfmR095Jy+mI6pqe0b5quG3VkTprY57z1P747hvQKBgDr1sK0llo2iqbLQP5r9/2WgosLJ3w6ykjVHriOhS0sM/2OJvwvrXu11RnQYvjFBQzZkaZ/T9y5oBLiqHnFK5pEnn+WygtcqhoW9ZVjzezPJjKDzqpATRX0mn54Cwcy/S4Yc6PsMmYuFNcfAxHITJwvy23BZ6u9t0cOX9VjkENCpAoGBAODLFsU1BQ0qC5vaWljzyjjwBBOmeAEzmvBzmLchQZqt7EGqts8N31/jc5t4wKLnwqhswjSz9RNdCu3/CMmbcBJxg6SkgwEfWyKITuzKWoOs9EvAAvI9PxTytGq8zWmPvmjUW6tYwP8neFZKclvd2lUW1gzgpK8nwNh48G34YSWV", "json", "utf-8",
-                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvwL3FKI6twK33wIVEElvQ6MHnqbv5bFJTajdKh6fA1wVLOBsxKyUuPVdsxNBCiLjG+DzhH2irSyMTKd+3Bl9p+ui/jtB7Iw1A38/GCMSEju6BQ1TpdD7Ek+oDUqQLrQp35sy+YZegKnjDLuNXgExqPfwa+wf4chYynNetaeIjHzzA6M2xTEv7iEIvqXqH6QutBFFjY/UZO8xVY/iI36ZYIAs3ctUEZTUw7NPTJgR/90InBMcyrmURp4UTKR1DxgksWGEtGvdgICvUscxX0thqMD6XyPRxxDV3KUpEBATxR+37EKPFnwtYdewQvpKQBltxX0y/4jN7JF0aC7ylgbZuwIDAQAB", "RSA2");
+                DefaultAlipayClient("https://openapi.alipaydev.com/gateway.do", "2018071960663535", "MIIEpQIBAAKCAQEA1BgpabZ9h54YExwXED9HlfMGbsA8wSATWMSbL2gn566+hHSq" +
+                "ooVgifIzu8t02hKa01IXU5RIBYBA0QGiyPpulMvd28WDzAbKLJRBrT4/pTBWU56G" +
+                "OBsiRaB7MJww5GQ5qoFzY/183JD+RiyX5cRlDusuq24sIRo9WHoKlApu1/BXQkE/" +
+                "mgnZj5jhDU/e6yLAMZhJ8//s2EXuaZ1u5s5OVBXMc2zxyG/8gxsxyuxogVxt1lIK" +
+                "fG0PtlkT6vzYZsnSQjgNugu2uuMa2vkT8QnVFm9CH7IJySkBmiOrCW2wTeWum6t8" +
+                "450qYDAUak+Ma4foQxQkHLiFlnGRozhHtkz2YwIDAQABAoIBAQCREyXgpW8gAMe+" +
+                "6ovsFRzGOMHBp8Tm5SXWpkTwHHKPSzwOJi9Bk7IBKaaxYe2/7NHykkhaZycMfDnZ" +
+                "vSC4EHTrWreayHPqbwqDi7WQ5u9p8rJZ6Cku34CF18vr6CqZTuUS+BER6CYsSRFk" +
+                "tsP/6mKl3cDCik2tJuGnT3NE6A4BrajaEBo7r6X3cI32xyAzkJiLbQ+r5tDuBmgP" +
+                "j+/CmiCqpVAPfZFveMJeCvsxxVTVC/Wq4pgx1SivUy+uz4xLSeraTjVvwu1UwkV5" +
+                "/7JK3i7xJlS/aUTG5RQPB58FCag2bCm3Ov7E55STF9cjf3jOWVuEXnYqiEbVUYIB" +
+                "CPnn+y5BAoGBAPgn8/iMYZIreenK7QtYED+L0KiQtVYJzSjGvUOQNzhM/rib5399" +
+                "+ufc+eIiMtn0+GgQ432PcDvwcS/47MFSPuiiE/BQC+SmbUPUEzqwj3T5Os20Tstc" +
+                "5f5U/ftUe8HMdWJOIzAkHSeqQwCRLPPFeIMAGoCqWk91ahmkb69IRR6bAoGBANrM" +
+                "ZvldsCCb7z01PsNunEpCMtZPLOlVP5t1AdfygTrHBwa/kplBPTgnUlkFNmyFlDp0" +
+                "Fj7HIRZbdXQMv2oNW3qEPm5tLCEp6XsJhKCzfThwmNDDY8SlioRU6R5DNOCAoxOz" +
+                "os8XCxekbROCYshyydQQAFzVckhTX3acMxhMd9/ZAoGAP5FcecrcI6lnbXBCEKna" +
+                "X6zdIpEPIp8YIt3mFayXuKBgj8HAJ3SKFWMRz8+a3yJVj7Lj04Iu6evJ0tQdMdhz" +
+                "rPv3x5N5uHS5gKqO0nXwCbLeUVti481ETUZQWVVm7loFp80EToyxuztuNnL55Ogs" +
+                "2sHoCBWbVMdrCm6I/YmxywkCgYEAtU556hvDjlIJHlxX5RRseHCAtewoGDZnHggI" +
+                "fgzbH3y7itEGxu4zr6uN5myWW7kZSpLVuDHEbkVdf8vsdhIZgt3A6jqXPegALM+H" +
+                "Uq9JjZJoX13594MB1ISUJFqmn36ZJANQX3EF5RKAvJDoEJhmPM8BrevF5r8HtT7c" +
+                "bD85eokCgYEAkOgLegw8jhYzizoJLhU/xQcL2z5BHrEJ9InLK4l/tgxi8IgCFqbq" +
+                "oejsmR5nepX+a7N/tIBxlZNj5aBUGJSumny5dGawtRGBFHwNwq0SSPYVRG5ODt5B" +
+                "veVO86Ip66EwLgXRBevMwzWebw/Ljct2Ol7Qd3XKeKu2QH5Enx56zwE=", "json", "utf-8",
+                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr9b6adnev/MJfFZaVcNQo9HSZfEnYVXdhqPI24xFaz1F1gVgYjTQbwFLBnxe1AGiUSfjqxhol7GUk8X+4f4EfceLrzDh2vwd/3XYKIHSNv1iNgm+HyymHjc6v8F563Ku8UibE6cUrUSlwpoSR8PaIvsih5k7+Z1vZ/bAVOQYMEFCBwzd5nHadfquz6pZfDsAaMR6qOJ6T8XMaITGk001P/Aat0Y9ksHq6Gx1NBg05D9miHRySFGz3WC+zJicsygX0AR5DpfwGeOT1FdgnLM6gxveBipXwz3xBaBMmp2e2p+N6CMSzh4rSK4pZaDTR9sHNkHJQ2oe+md2rlfpBFpSswIDAQAB", "RSA2");
 
         AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
 //SDK已经封装掉了公共参数，这里只需要传入业务参数。以下方法为sdk的model入参方式(model和biz_content同时存在的情况下取biz_content)。
         AlipayTradeAppPayModel model = new AlipayTradeAppPayModel();
         model.setBody("我是测试数据");
         model.setSubject("App支付测试Java");
-        model.setOutTradeNo("11111");
+        model.setOutTradeNo(String.valueOf(user_id));
         model.setTimeoutExpress("30m");
         model.setTotalAmount("0.01");
         model.setProductCode("QUICK_MSECURITY_PAY");
         request.setBizModel(model);
-        request.setNotifyUrl("商户外网可以访问的异步地址");
+        request.setNotifyUrl("http://114.115.143.130:8080/rechargeAlipay");
         try {
             //这里和普通的接口调用不同，使用的是sdkExecute
             AlipayTradeAppPayResponse response = alipayClient.sdkExecute(request);
@@ -73,9 +100,48 @@ public class AccountController {
         } catch (AlipayApiException e) {
             e.printStackTrace();
         }
-        return new ResponseEntity<String>("", HttpStatus.OK);
+        return new ResponseEntity<String>("success", HttpStatus.OK);
     }
+    @RequestMapping(value = "/rechargeAlipay", method = RequestMethod.POST)
+    String getResponse(HttpServletRequest request)
+    {
+        Map<String, String> params = new HashMap<String, String>();
+        Map<String, String[]> requestParams = request.getParameterMap();
+        for (Iterator<String> iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = iter.next();
+            String[] values = requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            params.put(name, valueStr);
+        }
+        boolean signVerified;
+        try {
+            signVerified = AlipaySignature.rsaCheckV1(params,"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr9b6adnev/MJfFZaVcNQo9HSZfEnYVXdhqPI24xFaz1F1gVgYjTQbwFLBnxe1AGiUSfjqxhol7GUk8X+4f4EfceLrzDh2vwd/3XYKIHSNv1iNgm+HyymHjc6v8F563Ku8UibE6cUrUSlwpoSR8PaIvsih5k7+Z1vZ/bAVOQYMEFCBwzd5nHadfquz6pZfDsAaMR6qOJ6T8XMaITGk001P/Aat0Y9ksHq6Gx1NBg05D9miHRySFGz3WC+zJicsygX0AR5DpfwGeOT1FdgnLM6gxveBipXwz3xBaBMmp2e2p+N6CMSzh4rSK4pZaDTR9sHNkHJQ2oe+md2rlfpBFpSswIDAQAB",  "UTF-8", "RSA2");
+        }
+        catch (com.alipay.api.AlipayApiException e)
+        {
+            return "fail";
+        }
 
+        if(signVerified)
+        {
+            if(request.getParameter("trade_status").equals("TRADE_SUCCESS")) {
+                int user_id = Integer.valueOf(request.getParameter("out_trade_no")).intValue();
+                int amount = (int)(Double.valueOf(request.getParameter("total_amount")).doubleValue()*100);
+                Account a = accountRepo.findOneById(user_id);
+                a.setMoney(a.getMoney()+amount);
+                accountRepo.save(a);
+                return "success";
+            }
+            return "fail";
+        }
+        else
+        {
+            return "fail";
+        }
+    }
 
     @Autowired
     private AddressRepo addressRepo;
